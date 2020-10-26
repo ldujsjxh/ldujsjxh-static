8934b7e4850749cb8be2d30cf9a9064586acf044
kr <1249815869@qq.com>
2020-10-26 22:10:12 +0800

    我将repair中的四个网页拆分成了两个大方面，一个是提交电脑问题哪个网页，一个是那三个电脑小知识的网页，义务维修指向的是那个表格的网页，另外三个网页需要学长在导航栏里增加一个指向的链接，链接指向的是快捷键那个页面。之后需要删掉一个工具链接。具体操作我写了一个word文档进行详细说明。

--- repair/js/repair.js
+++ repair/js/repair.js
@@ -1,34 +1,5 @@
-$(function() {
-    // 加载导航栏
-    $('#navigation').load('../navigation.html', function() {
-        // 导航栏修饰
-        // console.log('navbar', $('#navigation nav'));
-        $('#navigation nav').removeClass('fixed-top');
-        $('#navigation nav').removeClass('container');
-        $('#navigation nav').addClass('nav-container');
-        // 让屏幕下滚的距离大于导航栏的高度时，让导航栏始终出现在屏幕上方（使用flex布局），否则使用普通布局 --start
-        // 获取导航栏高度
-        var box_top = $("#bottom-of-nav").offset().top;
-        var flag = true;
-        $(window).scroll(function() {
-            if ($(document).scrollTop() >= box_top) {
-                // 卷去头部
-                $(".nav-container").addClass('fixed-top');
-                if (flag) {
-                    $(".nav-container").stop().fadeOut(0);
-                    $(".nav-container").stop().fadeIn();
-                    flag = false;
-                }
-            } else {
-                // 未卷去头部
-                $(".nav-container").removeClass('fixed-top');
-                flag = true;
-            }
-        });
-        // --end
-        //点击按钮隐藏或显示菜单栏, 暂时没用
-        $('.sidebar-toggler').click(function() {
-            $('.sidebar').toggleClass('shrink show');
-        });
-    });
+//# sourceURL=repair.js
+$(function () {
+    //引入头部导航栏
+    $("#navigation").load("../navigation.html");
 });
